b76b6b17d9bfd98e4a8ec57fa48d149194374501
pratiush pathak <pratiushpathak@gmail.com>
2025-05-01 00:46:20 +0530

    added array of object

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ const play = document.querySelector('.play');
 const next = document.querySelector('.next');
 const prev = document.querySelector('.previous');
 
+const songs = [
+  {
+    title: "Sooraj Dooba Hain",
+    artist: "Arijit Singh",
+    img: "images/sooraj.png", // replace with actual image
+    file:"audio/audio/Agar Tum Saath Ho From Tamasha-128kbps.mp3"
+  },
+  {
+    title: "Ik Vaari Aa",
+    artist: "Arijit Singh",
+    img: "images/ikvaari.png",
+    file:"audio/audio/Ik Vaari Aa From Raabta-128kbps.mp3"
+  },
+  {
+    title: "Agar Tum Saath Ho",
+    artist: "Alka Yagnik & Arijit Singh",
+    img: "images/agatumsathho.png",
+    file:"audio/audio/Sooraj Dooba Hain-128kbps.mp3"
+  }
+];
+
 
 // event listeners
 play.addEventListener('click', () => {
