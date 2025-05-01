d943c08f4cd57f82ffb9a448177a4fc81be23cc3
pratiush pathak <pratiushpathak@gmail.com>
2025-05-01 17:39:21 +0530

    project completed

--- script.js
+++ script.js
@@ -2,27 +2,47 @@ const audio = document.querySelector('.audio');
 const play = document.querySelector('.play');
 const next = document.querySelector('.next');
 const prev = document.querySelector('.previous');
+const title=document.querySelector('.title')
 
 const songs = [
   {
-    title: "Sooraj Dooba Hain",
+    title: "Agar tum saath ho",
     artist: "Arijit Singh",
     img: "images/sooraj.png", // replace with actual image
-    file:"audio/audio/Agar Tum Saath Ho From Tamasha-128kbps.mp3"
+    file:"audio/Agar Tum Saath Ho From Tamasha-128kbps.mp3"
   },
   {
     title: "Ik Vaari Aa",
     artist: "Arijit Singh",
     img: "images/ikvaari.png",
-    file:"audio/audio/Ik Vaari Aa From Raabta-128kbps.mp3"
+    file:"audio/Ik Vaari Aa From Raabta-128kbps.mp3"
   },
   {
-    title: "Agar Tum Saath Ho",
+    title: "Sooraj dooba hai",
     artist: "Alka Yagnik & Arijit Singh",
     img: "images/agatumsathho.png",
-    file:"audio/audio/Sooraj Dooba Hain-128kbps.mp3"
+    file:"audio/Sooraj Dooba Hain-128kbps.mp3"
   }
 ];
+function loadsong( song)
+
+{
+  title.innerText=song.title;
+  audio.src= song.file;
+ 
+
+}
+loadsong(songs[0]);
+
+let currentSongIndex=0;
+function loadSongByIndex(currentSongIndex)
+{
+  
+  loadsong(songs[currentSongIndex]);
+  audio.play();
+  play.innerHTML = `<img src="pause.png" alt="Pause" height="20px">`;
+
+}
 
 
 // event listeners
@@ -34,5 +54,13 @@ play.addEventListener('click', () => {
     audio.pause();
     play.innerHTML = `<img src="play-button-arrowhead.png" alt="Play" height="20px">`;
   }
+});
+next.addEventListener('click',()=>{
+  currentSongIndex=currentSongIndex+1;
+  loadSongByIndex(currentSongIndex);
+});
+prev.addEventListener('click',()=>{
+  currentSongIndex=currentSongIndex-1;
+  loadSongByIndex(currentSongIndex);
 })
 
